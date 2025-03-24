6f95215498956ec21d762772f4a528be6823faab
Sonu64 <sonusantu64@gmail.com>
2025-03-24 19:25:18 +0530

    Update createNumUsingDigits.cpp

--- Week2/createNumUsingDigits.cpp
+++ Week2/createNumUsingDigits.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 
-void createNum(int);
+int createNum(int);
 
 int main() {
-    int n;
+    int numOfDigits, number;
     cout << "Enter Number of digits: ";
-    cin >> n;
-    cout << "Digits in N: " << endl;
-    digits(n);
+    cin >> numOfDigits;
+    number = createNum(numOfDigits);
+    cout << "Your number is " << number << endl;
     return 0;
 }
 
 
-void digits(int n) {
-    int digit;
-    while(n>0) {
-        digit = n%10;
-        cout << digit << endl;
-        n /= 10;
+int createNum(int numOfDigits) {
+    int number = 0, digit = 0;
+    cout << "Enter " << numOfDigits << " digits one by one: " << endl;
+    for(int i=numOfDigits; i>0; i--) {
+        cin >> digit;
+        if (i == 1)
+            number += digit; // ex: For 3 digits it is d3*10^2 + d2*10^1 + d1*10^0
+        else 
+            number += digit * (int)pow(10, (i-1));
+        cout << endl;
+        cout << digit << ", " << number << endl;
     }
+    return number;
 }
